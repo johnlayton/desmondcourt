ec7453e8794d4b6d571173aa5bcc6daa753fbf01
john layton <johnlayton@aapt.net.au>
2012-03-28 11:09:14 +1100

    make the gap/separation configurable

--- public/javascripts/funnel.js
+++ public/javascripts/funnel.js
@@ -22,42 +22,37 @@ Funnel = function(elem, options) {
 
   this.data = this.options.data || []
 
-  var m = [25, 25, 25, 25],
-    w = 350 - m[1] - m[3],
-    h = 250 - m[0] - m[2],
-    y1 = 10,
-    y2 = (h - (y1 * (self.data.length - 1))) / self.data.length,
-    debug = false;
+  var y1 = this.options.gap || 10,
+      y2 = (self.size.height - (y1 * (self.data.length - 1))) / self.data.length;
 
   var svg = d3.select(this.funnel).append("svg:svg")
-    .attr("width",  w + m[1] + m[3])
-    .attr("height", h + m[0] + m[2])
-    .attr("border", "1")
+    .attr("width",  self.cx)
+    .attr("height", self.cy)
     .append("g")
-    .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+      .attr("transform", "translate(" + this.padding.left + "," + this.padding.top + ")");
 
-  if (debug) {
+  if (this.options.debug) {
     svg.append('svg:polygon')
       .attr('class',  'outline')
       .attr('points', function(d, i) {
         return [
           [ 0,     0 ].join(","),
-          [ w / 2, h ].join(","),
-          [ w,     0 ].join(",")
+          [ self.size.width / 2, self.size.height ].join(","),
+          [ self.size.width,     0 ].join(",")
         ].join(" ");
       })
       .attr('stroke-width', '1');
   }
 
-  var funnel = svg.append("svg:g").attr("class", "data");
+  var funnel = svg.append("svg:g")
+    .attr("class", "data");
 
   var chunks = funnel.selectAll("g.chunk")
     .data(self.data);
 
   var chunk = chunks.enter()
     .append("svg:g")
-    .attr("class", "chunk")
-    .on("click", function(d) { alert("Navigate to " + d.label + "\\n Count " + d.value);  });
+      .attr("class", "chunk");
 
   chunk.append("svg:polygon")
     .attr('fill', function(d) {
@@ -70,13 +65,13 @@ Funnel = function(elem, options) {
       var b2 = (i * (y1 + y2)) + y2;
 
       var d = function(x) {
-        return ((w * x) / (2 * h));
+        return ((self.size.width * x) / (2 * self.size.height));
       };
 
       return [
         [ d(b1),     b1 ].join(","),
-        [ w - d(b1), b1 ].join(","),
-        [ w - d(b2), b2 ].join(","),
+        [ self.size.width - d(b1), b1 ].join(","),
+        [ self.size.width - d(b2), b2 ].join(","),
         [ d(b2),     b2 ].join(",")
       ].join(" ");
     })
@@ -88,10 +83,10 @@ Funnel = function(elem, options) {
       return (i * (y1 + y2)) + (y2 / 2);
     })
     .attr("x", function(d) {
-      return w / 2;
+      return self.size.width / 2;
     })
     .attr("text-anchor", "middle")
-    .attr('stroke', 'darkblue')
+    .attr('stroke', '#335599')
     .text(function(d) {
       return d.label;
     });
